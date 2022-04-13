a1dee8acdc0ea58a65279b71cc8f9b22e1f27d92
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2022-04-13 21:24:20 +0700

    [UPDATE] horizontal navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar} from './components';
-
+import { Navbar } from "./components";
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-        Button
-      </button>
-      <Navbar/>
-
-      
+    <div className="min-h-screen">
+    <div className= "bg-slate-500">
+      <Navbar />
+     
     </div>
+   
+    
+  </div>
   );
 }
 
