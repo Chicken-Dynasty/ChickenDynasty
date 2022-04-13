48ee4e8f279288584c8b77be50316baf7cb2fa89
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2022-04-13 21:12:11 +0700

    [ADD] navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Navbar} from './components';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-  Button
-</button>
+        Button
+      </button>
+      <Navbar/>
+
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
