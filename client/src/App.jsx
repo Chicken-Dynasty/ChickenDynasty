b55ca3fac2cdba03c2b66b3882fefb1db666aa43
unn <62011286@kmitl.ac.th>
2022-05-20 18:05:12 +0700

    [WIP] Adding metamask connectivity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useState,useContext} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from "./components";
+import { TrasactionContext } from './context/transactionContext';
 const App = () => {
   const [count, setCount] = useState(0);
+  const { connectWallet} = useContext(TrasactionContext);
 
   return (
     <div className="min-h-screen">
     <Navbar />
-    
     
   </div>
   );
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+import {Loader} from './'
+import Navbar from './Navbar'
+
+const Welcome = () => {
+    return (
+        <h1> Welcome </h1>
+    )
+}
