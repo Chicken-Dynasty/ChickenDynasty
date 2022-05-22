afff6672d35fb42107f954274a5cf965d0ac3a35
Patthiya193 <79250844+Patthiya193@users.noreply.github.com>
2022-05-22 16:21:08 +0700

    [UPDATE] hatch to daisyUI

--- client/src/components/Hatch.jsx
+++ client/src/components/Hatch.jsx
@@ -7,11 +7,11 @@ const Hatch = () => {
     return(
         <div className="grid place-items-center h-screen bg-farm ">
             <div className=" bg-white/90 h-5/6 w-3/5 mt-5 ml-20  rounded-md  "> 
-                <div className="p-3 bg-yellow-300 rounded-md mb-2">
-                    <text className="font-alfa "> BUY AN EGG</text>
+                <div className="p-3 bg-rose-700 rounded-md mb-2">
+                    <text className="font-alfa text-black"> BUY AN EGG</text>
                 </div>
                 <div className="grid place-items-center mt-2">
-                     <text className=" font-alfa text-center"> Cost : 100 EGG</text>
+                     <text className=" font-alfa text-black text-center"> Cost : 100 EGG</text>
                 </div>
                 
                 
@@ -23,21 +23,23 @@ const Hatch = () => {
                         <img src="https://cdn.discordapp.com/attachments/958394721011662938/958394807460438047/Untitled2_20220329222043.png" alt="..." class="shadow rounded max-w-full h-auto align-center border-none" />
                     </div> */}
                 </div>
-                <div className="grid place-items-center p-3 bg-yellow-300 ">
+                <div className="grid place-items-center p-3 bg-rose-700 ">
                     <form>
-                        <text className="font-alfa"> Name your chicken :  </text>
-                        <input className="border-zinc-500" name= "cname" onChange={handleChickenName}></input>
+                        <text className="font-alfa text-black"> Name your chicken :  </text>
+                        <input className="border-zinc-500 text-black" name= "cname" onChange={handleChickenName}></input>
                     </form>
                 </div>
             </div>
 
-            <button
+            {/* <button
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
                 onClick={mintNFT}
                 className="inline-block px-6 py-2.5 bg-lime-600 text-white font-alfa text-md leading-tight rounded shadow-md hover:bg-lime-700 hover:shadow-lg focus:bg-lime-700 focus:shadow-lg focus:outline-none focus:ring-0  transition duration-150 ease-in-out ml-10 p-3"
-            > HATCH </button>
+            > HATCH </button> */}
+            <button class="btn btn-sm sm:btn-sm md:btn-md lg:btn-lg font-alfa text-white bg-lime-600 border-lime-600 hover:bg-lime-700 hover:shadow-lg focus:bg-lime-700 focus:shadow-lg hover:border-lime-700  transition duration-150"
+             onClick={mintNFT} >HATCH</button>
         </div>
     );
 };
