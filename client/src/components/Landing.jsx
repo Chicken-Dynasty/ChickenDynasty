44aade5363b0ff71195ac6c211f641e2b937cb88
gade <62011193@kmitl.ac.th>
2022-05-26 15:04:38 +0700

    [EDIT] carousel ml

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -9,8 +9,8 @@ const Landing = () => {
     console.log("landing");
     return(
         <div className="grid place-items-center h-screen bg-farm">
-            <h1 className="font-bold text-4xl text-black ml-20 mt-10">Cryptocurrencies</h1>
-            <div className="flex carousel w-auto ml-20 mb-0">
+            <h1 className="font-bold text-4xl text-black ml-64 mt-10">Cryptocurrencies</h1>
+            <div className="flex carousel w-auto ml-64 mb-0">
                 <div id="item1" className="carousel-item grid plac-items-center w-full ">
                     <CoinLineChart/>
                 </div> 
@@ -24,7 +24,7 @@ const Landing = () => {
                     <LineChart/>
                 </div>
             </div> 
-            <div className="flex justify-center w-full p-4 gap-2 ml-20 mt-0">
+            <div className="flex justify-center w-full p-4 gap-2 ml-64 mb-5">
                 <a href="#item1" className="btn btn-sm">1</a> 
                 <a href="#item2" className="btn btn-sm">2</a> 
                 <a href="#item3" className="btn btn-sm">3</a> 
